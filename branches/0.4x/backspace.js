fd7432f6afffa88b804e56592ce3a07e1bc7f0ae
me@noctarius.com <me@noctarius.com@9972fe1c-2dbb-11df-82ed-5f3c927ac887>
2010-03-19 09:45:02 +0000

    branched revision with 0.4x code

---
+++ branches/0.4x/backspace.js
@@ -0,0 +1,43 @@
+﻿document.onkeydown = BackspaceKeyListener;
+wasActivatedBefore = false;
+
+function BackspaceKeyListener(event) {
+	var isCtrl = event.ctrlKey;
+	var isAlt = event.altKey;
+	var isShift = event.shiftKey;
+	
+	if (!isCtrl && !isAlt) {
+		var target = event.target;
+		if (event.which == 8 && target) {		
+			// If on text fields or messagequeue
+			// was already triggered disable usage
+			if (target.type == 'text' ||
+					target.type == 'textarea') {
+				return true;
+			
+			} else {
+				// Mark as already triggered
+				window.setTimeout("UseBackspaceShortcut(" + isShift + ")", 0);
+				return false;
+			}
+		}
+	}
+	
+	return true;
+}
+function UseBackspaceShortcut(isShift) {
+	// Send message to background.html to test
+	// for activated state
+	chrome.extension.sendRequest( 
+		{ message: JSON.stringify( { command: "isActivated", data: location.href } ) }, 
+		function(response) {
+			console.log(response.message);
+			if (response.message == true)
+				if (!isShift)
+					window.history.back();
+				else
+					window.history.forward();
+		}
+	);
+}
+
